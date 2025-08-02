2225fa6168f559b311c5bfee6eaeed1a9fc50a02
emonmahdi <developeremon1997@gmail.com>
2025-08-02 12:17:52 +0600

    features in section done

---
+++ assets/js/logoSlider.js
@@ -0,0 +1,35 @@
+// const track = document.getElementById("logoTrack");
+// let interval = 3000;
+
+// setInterval(() => {
+//   const firstLogo = track.children[0];
+//   const logoWidth =
+//     firstLogo.offsetWidth + parseInt(getComputedStyle(firstLogo).marginRight);
+
+//   track.style.transition = "transform 0.6s ease-in-out";
+//   track.style.transform = `translateX(-${logoWidth}px)`;
+
+//   setTimeout(() => {
+//     track.style.transition = "none";
+//     track.appendChild(firstLogo);
+//     track.style.transform = "translateX(0)";
+//   }, 600);
+// }, interval);
+
+// JavaScript
+// const track = document.getElementById("logoTrack");
+// const interval = 3000;
+
+// setInterval(() => {
+//   const firstLogo = track.children[0];
+//   const logoWidth = firstLogo.offsetWidth + 24; // 24px is the horizontal gap
+
+//   track.style.transition = "transform 0.6s ease-in-out";
+//   track.style.transform = `translateX(-${logoWidth}px)`;
+
+//   setTimeout(() => {
+//     track.style.transition = "none";
+//     track.appendChild(firstLogo);
+//     track.style.transform = "translateX(0)";
+//   }, 600);
+// }, interval);
